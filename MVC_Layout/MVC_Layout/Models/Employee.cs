6fb02c39105b4e9837f1bd653ea1ba6aa2dcf2b5
Somanath Barge <somanath@nimap.com>
2023-08-08 17:16:02 +0530

    Tuesday 8/8/2023

---
+++ MVC_Layout/MVC_Layout/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Layout.Models
+{
+    public class Employee
+    {
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+    }
+}
