d76d09744b2d9bf2353f258030501073cbe110f3
Somanath Barge <somanath@nimap.com>
2023-08-23 16:17:07 +0530

    23/8/23

---
+++ MVC_Filters/MVC_Filters/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Filters.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        [HandleError]
+        public ActionResult Index()
+        {
+            throw new Exception();
+            return View();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+ try
+            {
+                int a = 10;
+                int b = 0;
+                int c = a / b;
+                ViewData["c"] = $"division is {c}";
+            }
+            catch (Exception ex)
+            {
+                ViewData["c"] = $"exception thrown";
+
+            }
+
+ try
+            {
+                throw new Exception(){ };
+            }
+            catch 
+            {
+                RedirectToAction("ErrorPage", "Home");
+            }
+            return View();
+        }
+        public ActionResult ErrorPage()
+        {
+            return View();
+        }
+ */
