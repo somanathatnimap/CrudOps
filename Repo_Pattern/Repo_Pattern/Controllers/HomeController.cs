3d2d722e68770696cca6a5c227fa60e54cba7f3c
Somanath Barge <somanath@nimap.com>
2023-08-30 18:23:53 +0530

    first

---
+++ Repo_Pattern/Repo_Pattern/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using Repo_Pattern.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Repo_Pattern.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        MainContext db=new MainContext();
+        public ActionResult Index()
+        {
+            var data=db.Student.Where(model=>model.Id!=0).ToList();
+            return View(data);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Student student)
+        {
+            db.Student.Add(student);
+            db.SaveChanges();
+            return View();
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var data=db.Student.Where(x=>x.Id==id).FirstOrDefault();
+            return View(data);
+        }
+        [HttpPost]
+        public ActionResult Edit(Student student)
+        {   if(ModelState.IsValid)
+            {
+                db.Entry(student).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+        return View();
+            
+        }
+        public ActionResult Delete(int id)
+        {
+            var data = db.Student.Where(x => x.Id == id).FirstOrDefault();
+            return View(data);
+        }
+        [HttpPost]
+        public ActionResult Delete(Student student)
+        {
+            db.Entry(student).State = EntityState.Deleted;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Repo_Pattern/Repo_Pattern/Models/MainContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Repo_Pattern.Models
+{
+    public class MainContext:DbContext 
+    {
+        public DbSet<Student> Student{ get; set; }
+    }
+}
